daf80589de94f2d355436a985c2864f022695cd0
Tanya Butenko <tanya@butenko.me>
2016-09-09 22:31:02 +1000

    main structure

---
+++ js/level1.js
@@ -0,0 +1,208 @@
+//Level 1
+
+/* Comments.
+    Let's start with comments - notes that people can read and computers will ignore.
+    They will help us up to guide you through the javaScript introduction journey.
+    There is a multi-line comments, like this one and also you can leave a single line comment
+    right in your code, example below. If you need to write some notes use comments starting with '//'
+*/
+
+//I'm your one-line comment
+
+
+/* Let's start with getting your code on the screen.
+    There is few ways you can do it and we will look into few of them:
+    - console.log('Hello World!'); - this line of code will print 'Hello World!' to the browser's console.
+    P.S: To see browser's console you can do right click in the window of you browser(Chrome, Firefox etc)
+    and select 'Inspect' or 'Inspect element' after that a console will appear on the bottom of the page.
+    - alert('Hello girls!'); - this line of code will pop-up a small window in your browser with text 'Hello girls!' in it,
+    but you need to refresh opened page first.
+ */
+
+
+//TODO: Now try to create an alert with any phrase you like.
+
+
+
+
+
+//TODO: After alert works for you, comment it out and refresh the page - it should not pop-up anymore.
+
+//TODO: What you say about trying console.log your message to browser? Send any message you like.
+
+
+
+
+
+/* Variables.
+    Variable is a place to store information. To create(declare) a variable use a keyword 'var', as follows:
+    var variableName;
+    So, we created a variable named variableName, but it has no information/value inside, it is empty. To give value(initialize)
+    to variable follow next step:
+    variableName = 'Hello world!';
+    We also can create and give value to variable in one step, as follows:
+    var newVariable = 1;
+    As you noticed we can give different types of values to our variables - strings, numbers, booleans etc.
+    String - is a set of characters, word or phrase that we wrap into quotes, see 'hello world!' in previous task.
+    Numbers - either integers or floats(decimals).
+    Boolean - it represents logical values - True or False
+ */
+
+//TODO: Now create two empty variable named numberOne and numberTwo
+
+
+
+
+/*
+    You also can use your variables to represent information that it has inside.
+    Example:
+    var hello = 'Hello World';
+    alert(hello);
+    It will pop-up alert box with string 'Hello World!'
+ */
+
+//TODO: Create 2 variables, 1 with your name and 2nd with your age and display them with alert pop-up box
+
+
+
+
+
+//TODO: Don't forget to comment alerts if you don't want them to pop-up every time
+
+
+/* Arithmetic operators
+    There is a bunch of different operators in programming. We will look through arithmetical operators now.
+    They are standard arithmetical operators (+, -, /, *, etc) that you can use to do math with your numbers.
+    Example:
+    var sumOfNumbers = 1 + 3;
+    alert(sumOfNumbers);
+    It will pop-up alert box with number 4
+ */
+
+//TODO: Create 3 variables, 1st variable named ten with value 10 inside, 2nd variable named three with value 3. And finally 3d variable named multipleOfNumbers that will be equal 1st variable multiply by 2nd variable. And in the end console or alert value of multipleOfNumbers
+
+
+
+
+
+/* Functions
+    Function is a separable, reusable piece of code. Some action that you want to do. It takes some input(arguments), do some manipulation
+    on it and return the output with key-word 'return'.
+    To create a function you need do following:
+    var functionName = function(argument){
+        return argument * 2;
+    };
+    So, our function will take 1 argument and return argument multiplied by 2. But for now it will do nothing as we need to call for our function.
+    To call the function we do so:
+    functionName(10);
+    Now we will call our function with argument that is 10. And our function will return us 20. To see what our function
+    returns us we can console.log it, for example.
+ */
+
+//TODO: It's your turn to create a function.
+//Step 1 - Name it 'add' and pass in two argumnets (num1 and num2).
+//Step 2 - This function should return us a summ of num1 and num2.
+//Step 3 - Call the function with numbers 2 and 3. To see result you can alert it or console.log it - to be sure that it works right.
+
+
+
+
+
+
+//TODO: Great, you made it! Now let's do another function named 'subtract' and pass 2 arguments num1 and num2.
+//Call on it with numbers 5 and 1 and console.log the result.
+//PS: do you know that instead of numbers you can create variables that store those numbers and pass them as an arguments to your function, try it up.
+
+
+
+
+
+
+
+
+/*If-else statements
+    What if we want our program to make a decision which function it should run? In this case we can use conditions.
+    If spider is big, action should be - run and save your life, but if it is tiny you can simply walk away.
+    Same with code, you can give 'if' condition to machine to make a decision what part of code to run.
+    Structure:
+    if(condition){
+        do this
+    } else {
+        do something else
+    }
+    Example:
+    var number = 7;
+    if (number > 7) {
+        console.log('Our number is bigger then 7');
+    } else {
+        console.log('Our number is smaller then 7');
+    };
+*/
+
+/*Comparison operators
+    You remember we were talking about arithmetical operators and that we have different operators in programming, so
+    here comes time to introduce you comparison operators that we can use to compare values(>, <, <=, =>, ==, !=).
+    Most of them you know from math classes in school, some of them can be new for you, so '==' is checking equality, if two values are equal.
+    '!=' - checks if they are not equal.
+    PS: Don't mix up '=' and '==' as they have different meaning.
+*/
+
+//TODO: So now we have 2 functions from previous task - add and subtract. Let's tell machine to decide what action to run
+//depending on arithmetical operator(+,-,/, * etc). If operator is '+' we should use add function, else - subtract function.
+//Step 1, create a variable called operator and let it be equal to '-'
+//Step 2, create if/else statement based on what operator we have,
+// if we have operator equal to '+' we call add function with any two numbers, else subtract function with any 2 numbers.
+//Don't forget to console.log it to see the result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*If - else if - else
+    Hm, but what if we have 4 arithmetical operations in our calculator? Well, we can use if - else if - else structure.
+    Example:
+    var number = 7;
+    if (number > 7) {
+        console.log('Our number is bigger then 7');
+    } else if (number < 7){
+        console.log('Our number is smaller then 7');
+    } else {
+        console.log('Our number is equal to 7');
+    };
+*/
+
+
+//TODO: Let's create 2 more functions and name them divide and multiply. After that let's extend our 'if else' check that we already created by
+// checking if it is equal to '/' - call 'divide' function, if it is '*' call multiply function,
+// else console.log - "Sorry, we don't know this operator".
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Congratulations! You have finished Part 1 of JavaScript Basics! Stand up, stretch your legs, celebrate your achievement.//
+//Next step will be following up the instructions in advanced.js file.                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ js/level2.js
@@ -0,0 +1,198 @@
+//Level2
+
+/* Arrays
+    It is ordered list of values. It can keep any number of values inside. And also
+    any type of values - numbers, strings, objects.
+    Example:
+    var animals = ['cat', 'dog', 'horse];
+    var randomThings = [2, 'book', 'today is Saturday', 10];
+    var numbers = [1, 2, 8, 19];
+ */
+
+//TODO: create your own array named favouriteFood and write in couple of things you like.
+
+
+
+
+
+/* Array length
+    We can easily check how many items we have in our array with property '.length'
+    Example:
+    var randomThings = [2, 'book', 'today is Saturday', 10];
+    console.log(randomThings.length); //we will get 4
+ */
+
+//TODO: check up on how many values you have in your array favouriteFood and console.log it
+
+
+
+
+
+
+/* Arrays usage
+    So, it is comfortable to keep all this values in one place, but what if we want to use
+    only 1 item from the array, we can get them out using 'bracket notation'.
+    As all counting in programming starts with 0, the first element in the array will have
+    position 0 etc
+    Example:
+    var randomThings = [2, 'book', 'today is Saturday', 10];
+    var firstItem = randomThings[0];
+    var secondItem = randomThings[1]; and so on
+ */
+
+//TODO: get 3d element from your array favouriteFood and console.log it
+
+
+
+
+
+
+
+/* Changing values in arrays
+    We also can replace values inside of the arrays by assigning specific item from
+    the array to a new value.
+    Example:
+    var animals = ['cat', 'dog', 'horse];
+    //let's replace 'dog' with 'fish'
+    animals[1] = 'fish';
+    //now our animals array will be ['cat', 'fish', 'horse]
+ */
+
+//TODO: take your array favouriteFood and replace first value with anything else.
+//console.log whole array to check
+
+
+
+
+
+
+
+/* Adding values to array
+    If you want to add new value to array you can use property of array named '.push'.
+    It will add value to the end of the array
+    Example:
+    var animals = ['cat', 'dog', 'horse];
+    animals.push('rabbit');
+ */
+
+//TODO: let's extend your list of favouriteFood and add one more value in it.
+//console.log list of your food o check
+
+
+
+
+
+
+
+
+/* Loops
+    People always have been lazy and it was moving progress forward. We don't like repeat
+    same boring actions again and again, so we are looking for the way how to improve it.
+    Programming is the same - for example, if I want to print 'JavaScript is awesome!' 10
+    times what are my options? Of course, I can print 10 lines of code repeating same
+    phrase over and over again, but I also can tell computer to do it instead of me.
+    To do it we have loops.
+    Each loop should have 3 main things:
+    - starting point
+    - condition (finishing point)
+    - counter (a step)
+    If you miss one of them you can get into infinite loop!!!
+ */
+
+
+/* While loops
+  'While' loop will do the action over and over again untill
+  some condition will be met.
+  For example:
+  var number = 0;   //starting point
+  while (number < 10) {      //10 is a condition/finishing point
+    console.log('JavaScript is awesome!');
+    number = number + 1;          // + 1 is a counter/size of the step
+  }
+ */
+
+//TODO: Using 'while loop' tell your computer to print you numbers from 10 to 1
+
+
+
+
+
+
+
+
+/* For loops
+    'For loop' is very similar to 'while loop', but you declare a counter in the statement.
+    For example:
+    for(var i = 0; i <= 5; i++){  //(starting point, condition, step)
+      console.log('Purr');
+    }
+    PS: i++ is a short from 'i = i + 1'
+ */
+
+//TODO: now, let's print every 3d number from 3 to 22 using 'for loop'
+
+
+
+
+
+
+
+/* Iterating through arrays
+    Now, when we know about loops I want to use each value from my animal
+    list and say that I love all of those animals, how shall I do it?
+    We can use 'for loop' to iterate through our array and get each value from it.
+    Example:
+    var animals = ['cats', 'dogs', 'horses];
+    for(var i = 0; i < animals.length; i++){
+      console.log('I love ' + animals[i]);
+    }
+ */
+
+//TODO: try it out with your list of favouriteFood that you created before.
+
+
+
+
+
+
+
+
+
+
+
+/* Loops and Logic
+    Now, when we know about if/else statements and loops let's combine them together
+    and make something more interesting. Let's count from 10 to 0 and print all the
+    numbers, but when we get to the middle(5) print ''WooHoo, we are in the middle'.
+    Example:
+    for(var i = 10; i > 0; i--){
+      if(i == 5){
+        console.log('WooHoo, we are in the middle');
+      } else {
+        console.log(i);
+      }
+    }
+ */
+
+//TODO: Time has come for a classic exercise - 'FizzBuzz'. Count from 1 to 50 and print
+// numbers out. If number multiple of 3 print 'Fizz', if multiple of 5 - 'Buzz', if
+// it is multiple of 3 and 5 - 'FizzBuzz'.
+//PS: you can use arithmetic operator modulo (%) - it is remainder when dividing.
+//10 % 3 = 1 - so in 10 we have 3*3 + 1
+//16 % 4 = 0 - as in 16 we have 4*4
+//19 % 4 = 3 - as in 19 we have 4*4 + 3 etc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
